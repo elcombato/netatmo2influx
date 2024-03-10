711e4a9b0eeb4f2ae484e8b106fc10ce5569d3e4
Jonas Hansen <jonas.hansen@airbus.com>
2024-03-10 22:12:22 +0100

    read actual measurements from netatmo - use interval with max resolution instead of latest value - thereby get measurements when actually measure instead of 10 minute interval

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 """Continously write mesaurements from Netatmo to InfluxDB
 """
+
 import os
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import pytz
 import lnetatmo
@@ -10,31 +11,163 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-def read_netatmo() -> dict:
-    """Read latest measurement via Netatmo API
+# interval between Netatmo queries in _minutes_
+READ_INTERVAL = 10
+NETATMO_TYPES = [
+    "temperature",
+    "humidity",
+    "co2",
+    "pressure",
+    "noise",
+    "rain",
+    "windstrength",
+]
+TZ = pytz.timezone("Europe/Berlin")
+
+
+def get_netatmo_data() -> lnetatmo.WeatherStationData:
+    """Read weather data via Netatmo API
 
     Returns:
-        dict: Netatmo measurement
+        lnetatmo.WeatherStationData: Netatmo Weather Station Data
     """
     auth = lnetatmo.ClientAuth()
-    weather_data = lnetatmo.WeatherStationData(auth)
-    latest_data = weather_data.lastData()
+
+    data = lnetatmo.WeatherStationData(auth)
 
     logging.info("Reading from Netatmo station(s):")
     station_info = [
-        f" - {st_name} in {st_data['place']['city']} {st_data['place']['location']}"
-        for st_name, st_data in weather_data.stations.items()
+        f" - {st_name} in {st_data['place']['city']}"
+        for st_name, st_data in data.stations.items()
     ]
     logging.info("\n".join(station_info))
-    return latest_data
+
+    return data
 
 
-def write_influxdb(netatmo_measure: dict):
-    """Write Netatmo Measurement to InfluxDB
+def netatmo2influx_single(weather_data: lnetatmo.WeatherStationData) -> list:
+    """Read latest values from Netatmo and parse into InfluxDB format
 
     Args:
-        netatmo_measure (dict): Netatmo measurement
+        weather_data (lnetatmo.WeatherStationData): Netatmo Weather Station Data
+
+    Returns:
+        list: List of InfluxDB points
     """
+    measurement = weather_data.lastData()
+
+    record_list = []
+    for room, r_d in measurement.items():
+        timestamp = datetime.fromtimestamp(r_d["When"], tz=TZ)
+        logging.info(" - %s: %s", str.rjust(room, 12), timestamp)
+        for measure, value in r_d.items():
+            if measure == "When":
+                continue
+            elif isinstance(value, str):
+                continue
+            record_list.append(
+                Point(room)
+                .field(measure, float(value))
+                .time(timestamp)
+            )
+
+    return record_list
+
+
+def __read_module(
+    module: dict, station_id: str, weather_data: lnetatmo.WeatherStationData
+) -> list:
+    """Read interval for single Netatmo module and parse into InfluxDB format
+
+    Args:
+        module (dict): Netatmo module info
+        station_id (str): MAC address of Netatmo station
+        weather_data (lnetatmo.WeatherStationData): Netatmo Weather Station Data
+
+    Returns:
+        list: List of InfluxDB points
+    """
+
+    # measurement types of module
+    m_types = [
+        str.lower(m)
+        for m in module["dashboard_data"].keys()
+        if str.lower(m) in NETATMO_TYPES
+    ]
+
+    logging.info("   - %s: %s", module["module_name"], m_types)
+
+    # measurements of module
+    record_list = []
+    for m_type in m_types:
+        start_date = datetime.now() - timedelta(minutes=READ_INTERVAL + 1)
+        measure = weather_data.getMeasure(
+            device_id=station_id,
+            module_id=module["_id"],
+            scale="max",
+            mtype=m_type,
+            date_begin=int((start_date).timestamp()),
+            date_end=int(datetime.now().timestamp()),
+        )
+
+        if len(measure["body"]) == 0:
+            logging.info("      No data for %s", m_type)
+            continue
+
+        record_list += [
+            Point(module["module_name"])
+            .field(m_type, float(val[0]))
+            .time(datetime.fromtimestamp(int(ts_epoch), tz=TZ))
+            for ts_epoch, val in measure["body"].items()
+        ]
+
+    # metadata of module
+    for m_type in ["battery_percent", "wifi_status", "rf_status"]:
+        if m_type in module:
+            ts_epoch = (
+                module["last_seen"]
+                if "last_seen" in module
+                else module["last_status_store"]
+            )
+            record_list.append(
+                Point(module["module_name"])
+                .field(m_type, float(module[m_type]))
+                .time(datetime.fromtimestamp(int(ts_epoch), tz=TZ))
+            )
+
+    return record_list
+
+
+def netatmo2influx_interval(weather_data: lnetatmo.WeatherStationData) -> list:
+    """Read interval of values from Netatmo and parse into InfluxDB format
+
+    Args:
+        weather_data (lnetatmo.WeatherStationData): Netatmo Weather Station Data
+
+    Returns:
+        list: List of InfluxDB points
+    """
+
+    influx_records = []
+
+    for station_id in weather_data.stationIds.keys():
+        station = weather_data.stationById(station_id)
+
+        influx_records += __read_module(station, station_id, weather_data)
+
+        for module in station["modules"]:
+            influx_records += __read_module(module, station_id, weather_data)
+
+    return influx_records
+
+
+def write_influxdb(record_list: list):
+    """Write to InfluxDB
+
+    Args:
+        record_list (list): List of InfluxDB points
+    """
+
     influx_url = os.getenv("INFLUX_URL")
     influx_token = os.getenv("INFLUX_TOKEN")
     influx_org = os.getenv("INFLUX_ORG")
@@ -47,18 +180,6 @@ def write_influxdb(netatmo_measure: dict):
         influx_url,
     )
 
-    tz = pytz.timezone("Europe/Berlin")
-    record_list = []
-    for room, r_d in netatmo_measure.items():
-        timestamp = datetime.fromtimestamp(r_d["When"], tz=tz)
-        logging.info(" - %s: %s", str.rjust(room, 12), timestamp)
-        for measure, value in r_d.items():
-            if measure == "When":
-                continue
-            elif isinstance(value, str):
-                continue
-            record_list.append(Point(room).field(measure, float(value)).time(timestamp))
-
     client = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
     write_api = client.write_api(write_options=SYNCHRONOUS)
     write_api.write(bucket=influx_bucket, record=record_list)
@@ -67,8 +188,6 @@ def write_influxdb(netatmo_measure: dict):
 
 if __name__ == "__main__":
 
-    # interval between Netatmo queries in _minutes_
-    INTERVAL = 5
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(message)s",
@@ -76,8 +195,9 @@ if __name__ == "__main__":
     )
     try:
         while True:
-            measurement = read_netatmo()
-            write_influxdb(measurement)
-            sleep(INTERVAL*60)
+            netatmo_data = get_netatmo_data()
+            influx_data = netatmo2influx_interval(netatmo_data)
+            write_influxdb(influx_data)
+            sleep(READ_INTERVAL * 60)
     except KeyboardInterrupt:
-        logging.warning('Interrupted')
+        logging.warning("Interrupted")
